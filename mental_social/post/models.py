a62fba9ab782a807897caa962f9c284f2231a98e
kareem magdy <kareemmagdy85888@gmail.com>
2023-06-25 23:08:50 +0300

    post model created

--- mental_social/post/models.py
+++ mental_social/post/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from users.models import Doctor
 
 # Create your models here.
+
+class post(models.Model):
+    creator = models.ForeignKey(Doctor,on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
