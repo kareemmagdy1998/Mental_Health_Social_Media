b2af21601f3578bf0cb6df56c91481e26b95a48b
kareem magdy <kareemmagdy85888@gmail.com>
2023-06-25 23:40:37 +0300

    chat model created

--- mental_social/chat/models.py
+++ mental_social/chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class ChatMessage(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    sender_is_doctor = models.BooleanField(default=False)
+    recipient_is_doctor = models.BooleanField(default=False)
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} to {self.recipient}: {self.message}'
--- mental_social/mental_social/settings.py
+++ mental_social/mental_social/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'users',
     'post',
+    'chat',
     
 ]
 
