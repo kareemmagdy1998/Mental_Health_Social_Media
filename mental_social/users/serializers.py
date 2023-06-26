b3b216e7883dfc222b2542659e823cc4777f90a8
kareem magdy <kareemmagdy85888@gmail.com>
2023-06-26 05:38:27 +0300

    add authentication using JWT

---
+++ mental_social/users/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields="__all__"
+        extra_kwargs = {
+            'password':{'write_only': True},
+        }
+        
+    def create(self, validation_data):
+            password = validation_data.pop('password', None) 
+            instance = self.Meta.model(**validation_data)
+            if password is not None:
+                instance.set_password(password)
+            instance.save()
+            return instance
+        
+
+    def update(self, instance, validated_data):
+            password = validated_data.pop('password', None)
+            instance = super().update(instance, validated_data)
+            if password is not None:
+                instance.set_password(password)
+                instance.save()
+            
+            return instance
+    
--- mental_social/users/views.py
+++ mental_social/users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http.response import JsonResponse
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+@api_view(['post'])
+def register(request):
+    serializer=UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
