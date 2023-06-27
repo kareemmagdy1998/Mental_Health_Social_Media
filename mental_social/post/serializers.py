c4b6c9ff4165d02658ec2edf8e8fbaea70c2ec74
Ahmed Osman <ahmadosm1998@gmail.com>
2023-06-27 15:51:01 +0300

    resolving migrations

--- mental_social/post/serializers.py
+++ mental_social/post/serializers.py
@@ -1,6 +1,23 @@
 from .models import Post, Comment
 from rest_framework import serializers
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    # replies = NestedCommentSerializer(many=True, required=False ,read_only=True)
+    # parent = serializers.IntegerField()
+    # replies = serializers.SerializerMethodField()
+    author = serializers.ReadOnlyField(source='author.username')  
+
+    # def get_replies(self, comment):
+        # replies = comment.replies.all()
+        # serializer = CommentSerializer(replies, many=True)
+        # return serializer.data
+    
+    class Meta:
+        model = Comment
+        fields = ('id', 'content', 'post', 'author', 'created_at', 'updated_at')
+
+
 class PostSerializer(serializers.ModelSerializer):
     creator = serializers.ReadOnlyField(source='creator.username')  
     creator_id = serializers.ReadOnlyField(source='creator.id')  
