08715f89bda8e82b71de407102a8a56c08545750
[abdelrahman sabry] <[abdelrhmansabry.219@gmail.com]>
2023-06-27 04:24:19 +0300

    CRUD-Comments

--- mental_social/post/urls.py
+++ mental_social/post/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from post.views import CommentListCreateView,CommentRetrieveUpdateDestroyView
+
 urlpatterns = [
-    # path('', ),
-]
+    path('<int:post_id>/comments/', CommentListCreateView.as_view(), name='comment-list-create'),
+    path('comments/<int:pk>/', CommentRetrieveUpdateDestroyView.as_view(), name='comment-retrieve-update-destroy'),
+    ]
