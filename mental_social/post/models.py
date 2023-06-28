05d8e226b22a730021723f9ba1928a97eb66f8ab
[abdelrahman sabry] <[abdelrhmansabry.219@gmail.com]>
2023-06-28 19:39:18 +0300

    Comment-pemissons

--- mental_social/post/models.py
+++ mental_social/post/models.py
@@ -6,21 +6,25 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 User = get_user_model()
 
+
 class Post(models.Model):
-    creator = models.ForeignKey(User,on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    
 
+    def __str__(self):
+        return f"Post by {self.creator.username} on {self.created_at}"
 
 
 class Comment(models.Model):
     content = models.TextField()
-    post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comment")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="replies"
+    )
 
     def __str__(self):
-        return f'Comment by {self.author.username} on {self.created_at}'    
+        return f"Comment by {self.author.username} on {self.created_at}"
