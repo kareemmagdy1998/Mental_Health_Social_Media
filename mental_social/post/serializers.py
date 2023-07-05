77d0187a68ae87c56d968c66008acfcb25ae2eb4
[abdelrahman sabry] <[abdelrhmansabry.219@gmail.com]>
2023-07-05 23:38:47 +0300

    comment author

--- mental_social/post/serializers.py
+++ mental_social/post/serializers.py
@@ -11,9 +11,11 @@ from .models import Post, Comment
 class CommentSerializer(serializers.ModelSerializer):
     # replies = NestedCommentSerializer(many=True, required=False ,read_only=True)
     author = serializers.ReadOnlyField(source='author.username')
+    author_id = serializers.ReadOnlyField(source='author.id')  
+
     class Meta:
         model = Comment
-        fields = ('id', 'content', 'post', 'author', 'created_at', 'updated_at')
+        fields = ('id', 'content', 'post', 'author','author_id' , 'created_at', 'updated_at')
         read_only_fields = ['author', 'created_at', 'updated_at']
 
 
